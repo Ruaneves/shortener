8f03650b123acade0c6c5d03791460ed1b033f2a
Ruan Neves <59776161+Ruaneves@users.noreply.github.com>
2022-11-10 21:28:48 +0000

    10/11 final?

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bp = require('body-parser');
-
+const dns = require('dns')
+const Database = require("@replit/database")
 
 const app = express();
+const db = new Database()
 
 const port = process.env.PORT || 3000;
 
@@ -16,19 +18,47 @@ app.use(cors());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  return res.sendFile(process.cwd() + '/views/index.html');
 });
 
 app.post('/api/shorturl', function(req, res) {
-  if (!req.body.url) return res.status(400).json({error:"Invalid URL"})
+  if (!req.body.url) return res.status(400).json({error:"Invalid URL"});
+  if (!/(http(s?)):\/\//i.test(req.body.url)) return res.status(400).json({error:"Invalid URL"});
 
-  res.json({
-    original_url: req.body.url,
-    short_url: Math.floor(Date.now() * 0.10),
-  });
+  
+  return dns.lookup(req.body.url.replace(/(http(s?)):\/\//i, ''), (err) => {
+    if (err) {
+      return res.status(400).json({error:"Invalid URL"});
+    } else {
+      var short = Math.floor(Date.now() * 0.10);
+      
+      db.get(short.toString()).then(value => {
+        if (!value) return;
+        short = Math.floor(Date.now() * 0.25);
+      });
+      
+      db.set(short.toString(), req.body.url);
+      
+      return res.json({
+        original_url: req.body.url,
+        short_url: short,
+      });
+    }
+  })
 
-  console.log(req.body)
 });
+
+app.get('/api/shorturl/:key', function(req, res) {
+  if (!req.params.key) return res.status(400).json({error:"Invalid URL"});
+  
+  return db.get(req.params.key.toString()).then(value => {
+    if (!value) return res.status(400).json({error:"Invalid URL"});
+    
+    console.log(`código ${req.params.key.toString()} acessado, redirecionando para ${value}`);
+  
+    return res.redirect(value);
+  })
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);

fd0cb8157eb81a286eba13250b34e87eba76dacd
dotRuan <ruaneves2486@gmail.com>
2022-11-10 17:41:27 -0300

    10/11

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bp = require('body-parser');
+
+
 const app = express();
 
-// Basic Configuration
 const port = process.env.PORT || 3000;
+
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 
 app.use(cors());
 
@@ -14,9 +19,15 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  if (!req.body.url) return res.status(400).json({error:"Invalid URL"})
+
+  res.json({
+    original_url: req.body.url,
+    short_url: Math.floor(Date.now() * 0.10),
+  });
+
+  console.log(req.body)
 });
 
 app.listen(port, function() {
